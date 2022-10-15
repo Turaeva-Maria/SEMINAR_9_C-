c1bc26167931cc571ccfc57729e85e31b1b6b8e2
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-15 10:31:51 +0300

    Программа на выведение всех чисел в промежутке через РЕКУРСИЮ

---
+++ task_63/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 63: Задайте значение N. Напишите программу, которая
+выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+string Print(int n)
+{
+   if (n == 1)
+      return $"{n} ";
+   return Print(n - 1) + $"{n} ";
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Print(n));
+
+
+
+
