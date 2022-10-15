85e9c05207a6b2327ba6e2f485a012b0c86a8c9d
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-15 11:02:25 +0300

    Программа на сумму цифр числа через РЕКУРСИЮ

---
+++ task_67/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 67: Напишите программу, которая будет принимать на вход
+число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+int Print(int n)
+{
+   if (n < 10) // цифра
+      return n;
+   return Print(n / 10) + n % 10;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Print(n));
+
